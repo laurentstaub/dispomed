c5717bac4f9c4746a593f6e0c3ce9496dc3ec663
laurentstaub <laurent.staub@gmail.com>
2024-07-15 16:55:13 +0200

    added colored tooltip

--- src/d3Dashboard.js
+++ src/d3Dashboard.js
@@ -28,7 +28,7 @@ d3.csv("/data/incidents.csv").then(data => {
     }
   });
 
-  start_date_chart = new Date(2022, 0, 1);
+  start_date_chart = new Date(2023, 0, 1);
   end_date_chart = new Date(d3.max(data, d => d.end_date).getFullYear(), 11, 31);
   date_last_report = d3.max(data, d => d.end_date);
   //date_last_report.setHours(0, 0, 0, 0);
@@ -157,31 +157,59 @@ function drawBarChart(data, isInitialSetup) {
     .style("text-anchor", "middle");
 
 // Y-AXIS
+  // innerChart
+  //   .append("g")
+  //   .call(d3.axisLeft(yScale).tickSize(0))
+  //   .selectAll(".tick text")
+  //   .attr("x", -margin.left)
+  //   .style("text-anchor", "start")
+  //   .text(function(d) {
+  //     return d.length > labelMaxLength ? d.substring(0, labelMaxLength) + "..." : d;
+  //   })
+  //   .on("mouseover", function(event, d) {
+  //     const product = data.find(item => item.product === d);
+  //     if (d.length > labelMaxLength || product) {
+  //       const tooltip = d3.select("#tooltip");
+  //       tooltip.transition().duration(200).style("opacity", 0.9);
+  //       tooltip.html(`
+  //         <strong>Produit:</strong> ${d}<br>
+  //         <strong>Statut:</strong> ${getProductStatus(product)}
+  //       `)
+  //         .style("left", (event.pageX + 10) + "px")
+  //         .style("top", (event.pageY - 28) + "px");
+  //     }
+  //   })
+  //   .on("mouseout", function() {
+  //     d3.select("#tooltip").transition().duration(500).style("opacity", 0);
+  //   });
+
   innerChart
-    .append("g")
-    .call(d3.axisLeft(yScale).tickSize(0))
-    .selectAll(".tick text")
-    .attr("x", -margin.left)
-    .style("text-anchor", "start")
-    .text(function(d) {
-      return d.length > labelMaxLength ? d.substring(0, labelMaxLength) + "..." : d;
-    })
-    .on("mouseover", function(event, d) {
-      const product = data.find(item => item.product === d);
-      if (d.length > labelMaxLength || product) {
-        const tooltip = d3.select("#tooltip");
-        tooltip.transition().duration(200).style("opacity", 0.9);
-        tooltip.html(`
-          <strong>Produit:</strong> ${d}<br>
-          <strong>Statut:</strong> ${getProductStatus(product)}
-        `)
-          .style("left", (event.pageX + 10) + "px")
-          .style("top", (event.pageY - 28) + "px");
-      }
-    })
-    .on("mouseout", function() {
-      d3.select("#tooltip").transition().duration(500).style("opacity", 0);
-    });
+      .append("g")
+      .call(d3.axisLeft(yScale).tickSize(0))
+      .selectAll(".tick text")
+      .attr("x", -margin.left)
+      .style("text-anchor", "start")
+      .text(function(d) {
+          return d.length > labelMaxLength ? d.substring(0, labelMaxLength) + "..." : d;
+      })
+      .on("mouseover", function(event, d) {
+          const product = data.find(item => item.product === d);
+          if (d.length > labelMaxLength || product) {
+              const status = getProductStatus(product);
+              const tooltip = d3.select("#tooltip");
+              tooltip.transition().duration(200).style("opacity", 0.9);
+              tooltip.html(`
+                  <strong>Produit:</strong> ${d}<br>
+                  <strong>Statut:</strong> ${status.text}
+              `)
+              .attr("class", status.class)
+              .style("left", (event.pageX + 10) + "px")
+              .style("top", (event.pageY - 28) + "px");
+          }
+      })
+      .on("mouseout", function() {
+          d3.select("#tooltip").transition().duration(500).style("opacity", 0);
+      });
 
 // GRID
   // Add horizontal grid lines manually after bars to ensure they are on top
@@ -207,17 +235,6 @@ function drawBarChart(data, isInitialSetup) {
     .attr("y1", 0)
     .attr("y2", 0)
 
-  // Add vertical lines for each year beginning
-  innerChart.selectAll(".year-line")
-    .data(yearTicks)
-    .enter()
-    .append("line")
-      .attr("class", "year-line")
-      .attr("x1", d => xScale(d))
-      .attr("x2", d => xScale(d))
-      .attr("y1", 0)
-      .attr("y2", innerHeight)
-
   // Add horizontal line on top of products
   innerChart.append("line")
     .attr("class", "year-line")
@@ -237,13 +254,24 @@ function drawBarChart(data, isInitialSetup) {
       .attr("y1", 0)
       .attr("y2", innerHeight)
 
-  // Add an additional vertical line at the end of the x-axis
-  innerChart.append("line")
-    .attr("class", "year-line")
-    .attr("x1", innerWidth)
-    .attr("x2", innerWidth)
-    .attr("y1", 0)
-    .attr("y2", innerHeight)
+  // // Add vertical lines for each year beginning
+  // innerChart.selectAll(".year-line")
+  //   .data(yearTicks)
+  //   .enter()
+  //   .append("line")
+  //     .attr("class", "year-line")
+  //     .attr("x1", d => xScale(d))
+  //     .attr("x2", d => xScale(d))
+  //     .attr("y1", 0)
+  //     .attr("y2", innerHeight)
+
+  // // Add an additional vertical line at the end of the x-axis
+  // innerChart.append("line")
+  //   .attr("class", "end-line")
+  //   .attr("x1", innerWidth)
+  //   .attr("x2", innerWidth)
+  //   .attr("y1", 0)
+  //   .attr("y2", innerHeight)
 
   d3.select("#search-box").on("input", function() {
     filterProducts(this.value, filteredData);
@@ -316,19 +344,36 @@ function filterProducts(searchTerm, data) {
   drawBarChart(filterData, false);
 }
 
+// function getProductStatus(d) {
+//   if (d.status === "arret") {
+//     return "Arrêt de commercialisation";
+//   } else if (d.start_date <= date_last_report && d.end_date >= date_last_report) {
+//     if (d.status === "Rupture") {
+//       return "Rupture de stock";
+//     } else if (d.status === "Tension") {
+//       return "Tension d'approvisionnement";
+//     } else if (d.status === "Arret") {
+//       return "Arrêt de commercialisation";
+//     }
+//   } else if (!d.end_date || d.end_date < date_last_report) {
+//     return "Disponible";
+//   }
+//   return "Statut inconnu";
+// }
+//
 function getProductStatus(d) {
-  if (d.status === "arret") {
-    return "Arrêt de commercialisation";
-  } else if (d.start_date <= date_last_report && d.end_date >= date_last_report) {
-    if (d.status === "Rupture") {
-      return "Rupture de stock";
-    } else if (d.status === "Tension") {
-      return "Tension d'approvisionnement";
-    } else if (d.status === "Arret") {
-      return "Arrêt de commercialisation";
+    if (d.status === "arret") {
+        return { text: "Arrêt de commercialisation", class: "tooltip-arret" };
+    } else if (d.start_date <= date_last_report && d.end_date >= date_last_report) {
+        if (d.status === "Rupture") {
+            return { text: "Rupture de stock", class: "tooltip-rupture" };
+        } else if (d.status === "Tension") {
+            return { text: "Tension d'approvisionnement", class: "tooltip-tension" };
+        } else if (d.status === "Arret") {
+          return { text: "Arrêt de commercialisation", class: "tooltip-arret" };
+        }
+    } else if (!d.end_date || d.end_date < date_last_report) {
+        return { text: "Disponible", class: "tooltip-disponible" };
     }
-  } else if (!d.end_date || d.end_date < date_last_report) {
-    return "Disponible";
-  }
-  return "Statut inconnu";
+    return { text: "Statut inconnu", class: "" };
 }
