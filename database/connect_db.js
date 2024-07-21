eba273427ebcda83238c5fab38f77c6123d17c47
laurentstaub <laurent.staub@gmail.com>
2024-07-21 15:25:42 +0200

    added gitignore

---
+++ database/connect_db.js
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'your_username',
+  host: 'localhost',
+  database: 'your_database',
+  password: 'your_password',
+  port: 5432,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
